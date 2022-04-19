68537f2c080b82888fce393b3cda1f5918d46d12
GotoRen <r0719en@pluslab.org>
2022-04-19 22:32:17 +0900

    ✨ [Add] nats configs.

---
+++ nats/port-restricted.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i eth1 -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i eth1 -p udp -m state --state NEW -j DROP
+iptables -A FORWARD -i eth1 -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i eth1 -p udp -m state --state NEW -j DROP
+
+iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination 192.168.1.10
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source 10.0.100.200
+
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
